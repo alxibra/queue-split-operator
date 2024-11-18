f7a46c4466bd816d860d08fadcb43211968bf65d
alxibra <alxibra@forstok.com>
2024-11-18 09:22:07 +0700

    create replicaset

--- internal/controller/queuesplit_controller.go
+++ internal/controller/queuesplit_controller.go
@@ -20,7 +20,9 @@ import (
 	"context"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -69,12 +71,59 @@ func (r *QueueSplitReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	err = r.Get(context.Background(), types.NamespacedName{Name: queuesplit.Name, Namespace: queuesplit.Namespace}, existingRs)
 	if err != nil && errors.IsNotFound(err) {
 		lg.Info("ReplicaSet not found\n")
+		rs := buildReplicaSet()
+		err = r.Create(context.Background(), rs)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 		return ctrl.Result{}, nil
 	}
 	lg.Info("ReplicaSet found\n")
 
 	return ctrl.Result{}, nil
 }
+
+func buildReplicaSet() *appsv1.ReplicaSet {
+	rs := &appsv1.ReplicaSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: appsv1.ReplicaSetSpec{
+			Replicas: int32Ptr(3), // Desired number of replicas
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "test-app",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "test-app",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "test-container",
+							Image: "nginx:1.21.1",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return rs
+}
+
+func int32Ptr(i int32) *int32 { return &i }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *QueueSplitReconciler) SetupWithManager(mgr ctrl.Manager) error {
